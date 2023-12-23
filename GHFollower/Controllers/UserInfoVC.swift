882cb480f0b044156e971cfabcf14c54adc7efb4
zuka papuashvili <zurabpapuashvili@gmail.com>
2023-12-23 23:40:49 +0400

    last commit.

--- GHFollower/Controllers/UserInfoVC.swift
+++ GHFollower/Controllers/UserInfoVC.swift
@@ -8,22 +8,54 @@
 import UIKit
 
 class UserInfoVC: UIViewController {
+    
+    let headerView = UIView()
+    
+    var username: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let user):
+                DispatchQueue.main.async {
+                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
+            case.failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something Went Wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+        
+        layoutUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+                
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
     }
-    */
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
+    }
 
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
 }
--- GHFollower/Utilities/Constants.swift
+++ GHFollower/Utilities/Constants.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
+
